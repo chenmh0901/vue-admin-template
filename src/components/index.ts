525d28ab1b1f7495d551c7eda7a7d60be4c9da37
chenmh0901 <1210204146@zust.edu.cn>
2024-05-20 06:50:05 -0400

    build

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,16 +1,22 @@
+import type { App, Component } from 'vue' // Import the necessary types
+
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import SvgIcon from './SvgIcon/index.vue'
 import Category from './Category/index.vue'
 
-const allGlobalComponents = { SvgIcon, Category }
+interface GlobalComponents {
+  [key: string]: Component
+}
+
+const allGlobalComponents: GlobalComponents = { SvgIcon, Category }
 
 export default {
-  install(app) {
+  install(app: App) { // Provide explicit type for the app parameter
     Object.keys(allGlobalComponents).forEach((key) => {
       app.component(key, allGlobalComponents[key])
     })
 
     for (const [key, component] of Object.entries(ElementPlusIconsVue))
-      app.component(key, component)
+      app.component(key, component as Component) // Provide explicit type for the component
   },
 }
